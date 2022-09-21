ead8c81e1bb8c7e39b03854788deb4ee79c16b9b
Usman Afzal <56322052+UsmanAfzal8@users.noreply.github.com>
2022-09-21 21:25:08 +0500

    name changed

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Window Connect With Firebase',
       theme: ThemeData(
         // This is the theme of your application.
         //
